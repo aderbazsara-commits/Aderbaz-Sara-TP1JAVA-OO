65151ea499ba7dfceac6a795e15ffec52ef55e23
aderbazsara-commits <aderbazsara@gmail.com>
2026-03-16 13:39:28 +0000

    Delete MainPersonne.java

--- MainPersonne.java
+++
@@ -1,9 +0,0 @@
-//EXO1-QST4
-//4.Écrire une fonction main (dans une autre class) pour implémenter un exemple d’utilisation
-public class MainPersonne {
-    public static void main(String[] args) {
-        Personne A = new Personne();
-        A.initialiser("BW0000", "Aderbaz", "Sara", "aderbazsara@gmail.com", 21);
-        A.afficherPersonne();
-    }
-}
